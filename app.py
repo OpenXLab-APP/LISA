542327ed45bee54d702be117a092896052c69e06
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-08-11 15:15:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ import os
 if not os.path.exists('./bitsandbytes'):
     os.system("git clone https://github.com/timdettmers/bitsandbytes.git && cd bitsandbytes && CUDA_VERSION=113 make cuda11x && python setup.py install && cd .. && rm -r bitsandbytes && export PYTHONPATH=./ && python3 run.py && python git lfs install")
  
+git lfs install
+git lfs clone https://huggingface.co/spaces/xinlai/LISA/tree/main/LISA-13B-llama2-v0-explainatory
