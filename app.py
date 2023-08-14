2bc685eb7c5f841b7a0206c809f5a11a513bbe69
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-08-14 16:41:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ if not os.path.exists('./bitsandbytes'):
 
 from openxlab.model import download
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-text_hidden_fcs.bin')
+model_name='pytorch_model-text_hidden_fcs.bin', output='./LISA-13B-llama2-v0-explainatory')
 
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00001-of-00003.bin')
+model_name='pytorch_model-00001-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
 
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00002-of-00003.bin')
+model_name='pytorch_model-00002-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
 
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00003-of-00003.bin')
+model_name='pytorch_model-00003-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
 
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-visual_model.bin')
+model_name='pytorch_model-visual_model.bin', output='./LISA-13B-llama2-v0-explainatory')
