0e24535e5072d4566e985318327c5f6875b3b21b
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-08-14 15:59:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import os
 if not os.path.exists('./bitsandbytes'):
     os.system("git clone https://github.com/timdettmers/bitsandbytes.git && cd bitsandbytes && CUDA_VERSION=113 make cuda11x && python setup.py install && cd .. && rm -r bitsandbytes && export PYTHONPATH=./ && python3 run.py")
 
-import openxlab
-openxlab.login(ak=<q9bpknwq8valkv1xmvno>, sk=<gr24e76lkxoa1n0medzykgeygwddlvoq9bym58zg>)
-
 from openxlab.model import download
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
 model_name='pytorch_model-text_hidden_fcs.bin')
