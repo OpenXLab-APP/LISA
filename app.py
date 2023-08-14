b91110e464cf9fa568137a0b140a5e86602a86ec
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-08-14 17:10:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,7 @@ model_name='pytorch_model-00003-of-00003.bin', output='./LISA-13B-llama2-v0-expl
 download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
 model_name='pytorch_model-visual_model.bin', output='./LISA-13B-llama2-v0-explainatory')
 
+print(args.version)
+
 if not os.path.exists('./bitsandbytes'):
     os.system("git clone https://github.com/timdettmers/bitsandbytes.git && cd bitsandbytes && CUDA_VERSION=113 make cuda11x && python setup.py install && cd .. && rm -r bitsandbytes && export PYTHONPATH=./ && python3 run.py")
