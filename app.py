3dda680e530703fa4f2f3cb0800654a8fb497d35
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-08-14 19:50:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,8 @@ import bleach
 import sys
 import os
 
-from openxlab.model import download
-download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-text_hidden_fcs.bin', output='./LISA-13B-llama2-v0-explainatory')
-
-download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00001-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
-
-download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00002-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
-
-download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-00003-of-00003.bin', output='./LISA-13B-llama2-v0-explainatory')
-
-download(model_repo='openxlab-app/pytorch_model-text_hidden_fcs.bin', 
-model_name='pytorch_model-visual_model.bin', output='./LISA-13B-llama2-v0-explainatory')
+from huggingface_hub import snapshot_download
+snapshot_download(repo_id="xinlai/LISA", local_dir='./LISA-13B-llama2-v0-explainatory')
 
 if not os.path.exists('./bitsandbytes'):
     os.system("git clone https://github.com/timdettmers/bitsandbytes.git && cd bitsandbytes && CUDA_VERSION=113 make cuda11x && python setup.py install && cd .. && rm -r bitsandbytes && export PYTHONPATH=./ && python3 run.py")
